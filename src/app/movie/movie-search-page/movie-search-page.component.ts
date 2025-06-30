e66b4ad382d7b83accfe28fe5e4c8756fed1d120
Julian Jandl <julian@jandl.wien>
2025-06-30 13:38:55 +0200

    feat: implement exercise 6 - rxResource

--- src/app/movie/movie-search-page/movie-search-page.component.ts
+++ src/app/movie/movie-search-page/movie-search-page.component.ts
@@ -1,32 +1,41 @@
-import { Component, computed, inject } from '@angular/core';
-import { toSignal } from '@angular/core/rxjs-interop';
-import { ActivatedRoute } from '@angular/router';
-import { Observable, switchMap } from 'rxjs';
+import { Component, inject } from '@angular/core';
+import { rxResource } from '@angular/core/rxjs-interop';
+import { injectParams } from 'ngxtension/inject-params';
 
-import { TMDBMovieModel } from '../../shared/model/movie.model';
 import { MovieService } from '../movie.service';
 import { MovieListComponent } from '../movie-list/movie-list.component';
 
 @Component({
   selector: 'movie-search-page',
   template: `
-    @if (showLoader()) {
-      <movie-list [movies]="movies()" />
-    } @else {
+    @if (movieResource.hasValue()) {
+      <movie-list [movies]="movieResource.value()!" />
+    }
+
+    @if (movieResource.isLoading()) {
       <div class="loader"></div>
+    }
+
+    @if (movieResource.error()) {
+      <div class="error">
+        There are no movies to show.
+        {{ movieResource.error() }}
+      </div>
     }
   `,
   imports: [MovieListComponent],
 })
 export class MovieSearchPageComponent {
   private movieService = inject(MovieService);
-  private activatedRoute = inject(ActivatedRoute);
 
-  movies$: Observable<TMDBMovieModel[]> = this.activatedRoute.params.pipe(
-    switchMap((params) => this.movieService.searchMovies(params['query'])),
-  );
+  searchQuery = injectParams('query');
 
-  movies = toSignal(this.movies$, { initialValue: [] });
-
-  showLoader = computed(() => this.movies().length === 0);
+  movieResource = rxResource({
+    // request -> params in version 20
+    request: this.searchQuery,
+    loader: (params) => {
+      const searchQuery = params.request;
+      return this.movieService.searchMovies(searchQuery!);
+    },
+  });
 }
